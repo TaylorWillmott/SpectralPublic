f2085435df452a01ffec86aa39313d771f55c2fd
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2019-02-26 19:28:32 +0000

    govJobs List Added
    Added a list of all government jobs for use in other addons.

--- spectralrp base/lua/autorun/server/sv_init.lua
+++ spectralrp base/lua/autorun/server/sv_init.lua
@@ -4,6 +4,19 @@ local base = {
 	
 	base = true,
 	
+	govJobs = { 
+				TEAM_MAYOR	= "Mayor",
+				TEAM_SECRET	= "Secret Service",
+				TEAM_SWATL	= "SWAT Leader",
+				TEAM_SWATB	= "SWAT Bomb Disposal",
+				TEAM_SWATM	= "SWAT Medic",
+				TEAM_SWATD	= "SWAT Breacher",
+				TEAM_SWATS	= "SWAT Sniper",
+				TEAM_SWAT	= "SWAT",
+				TEAM_CHIEF	= "Police Chief",
+				TEAM_POLICE	= "Police",
+				},
+	
 };
 
 SpectralRP = base
