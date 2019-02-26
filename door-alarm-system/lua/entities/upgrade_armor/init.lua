cb0a10b6b7ba555a9be246c7d97222022d09bc12
Taylor W <43759113+TheRandomnessGuy@users.noreply.github.com>
2019-02-26 16:04:27 +0000

    Altered Armour's Functionality
    Armour now reduces damage taken by a certain percentage rather than just adding more health.

--- door-alarm-system/lua/entities/upgrade_armor/init.lua
+++ door-alarm-system/lua/entities/upgrade_armor/init.lua
@@ -2,8 +2,5 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 ENT.applyUpgrade = function(ar, alarm)
-	alarm:SetMaxHealth(alarm:GetMaxHealth() +alarm_system.settings.health.armor)
-	alarm:SetHealth(alarm:GetMaxHealth())
-	
-	ar:Remove() --no visual representation so let's remove it...
+	ar:Remove()
 end
